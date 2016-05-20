cfd66df10acf899f3e9b4bb3c4d38a3d66e13ce8
byt3bit <arnab.roy@windowslive.com>
2016-05-20 12:57:25 +0200

    NTL header files

---
+++ common/ntl_header.hpp
@@ -0,0 +1,17 @@
+#ifndef NTL_HEADER_HPP_
+#define NTL_HEADER_HPP_
+#include <NTL/GF2.h>
+#include <NTL/GF2E.h>
+#include <NTL/GF2EX.h>
+#include <NTL/GF2X.h>
+#include <NTL/GF2XFactoring.h>
+#include <NTL/GF2EXFactoring.h>
+#include <NTL/vec_GF2.h>
+#include <NTL/mat_GF2.h>
+#include <NTL/ZZ.h>
+#include <NTL/vector.h>
+#include <NTL/ZZ_p.h>
+#include <NTL/ZZ.h>
+#include <NTL/matrix.h>
+#include <NTL/mat_GF2E.h>
+#endif
